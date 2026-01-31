95b2965a9652d4e6b32891632c71fc504616f204
João Pedro Sanches <joaopedrosanches370@gmail.com>
2026-01-31 17:44:35 -0300

    Criando a classe posicao xadrez

---
+++ xadrez-console/xadrez/PosicaoXadez.cs
@@ -0,0 +1,21 @@
+﻿using tabuleiro;
+
+namespace xadrez {
+    class PosicaoXadez {
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+    }
+}
