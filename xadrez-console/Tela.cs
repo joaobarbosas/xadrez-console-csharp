1d7ba09dbf33ec36567d81afca6051443107be31
João Pedro Sanches <joaopedrosanches370@gmail.com>
2026-02-06 00:02:54 -0300

    Implementando a logica de xeque-mate

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -12,10 +12,14 @@ namespace xadrez_console {
             imprimirPecasCapturadas(partida);
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
-            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
-                if (partida.terminada) {
-                    Console.WriteLine("XEQUE-MATE!");
-                    Console.WriteLine("Vencedor: " + partida.jogadorAtual);
+            if (!partida.terminada) {
+                Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            }
+            if (partida.xeque) {
+                Console.WriteLine("XEQUE!");
+            } else {
+                Console.WriteLine("XEQUE-MATE!");
+                Console.WriteLine("Vencedor: " + partida.jogadorAtual);
             }
         }
 
